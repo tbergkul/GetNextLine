470e454fe6156907add2fd5672521322b2be84c7
tbergkul <tbergkul@student.hive.fi>
2019-10-31 14:43:17 +0200

    All necessary files created and uploaded

---
+++ get_next_line.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbergkul <tbergkul@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/31 10:41:44 by tbergkul          #+#    #+#             */
+/*   Updated: 2019/10/31 10:41:44 by tbergkul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include "./libft/libft.h"
+#include <stdio.h>
+
+int	get_next_line(const int fd, char **line)
+{
+	return (-1 (error has happened));
+	return (1 (line has been read));
+	return (0 (reading has been completed));
+}
+
+int	main(int ac, char **av)
+{
+	char **line = {};
+
+	get_next_line(1, line);
+	return (0);
+}
---
+++ get_next_line.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbergkul <tbergkul@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/31 10:42:41 by tbergkul          #+#    #+#             */
+/*   Updated: 2019/10/31 10:42:41 by tbergkul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
+
+int	get_next_line(const int fd, char **line);
+
+#endif
