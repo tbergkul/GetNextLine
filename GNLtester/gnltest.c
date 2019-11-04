7a7aaed3d49a1bde6e4f7f358a814f1efa920afe
tbergkul <tbergkul@student.hive.fi>
2019-11-04 15:46:17 +0200

    Modified files

--- GNLtester/gnltest.c
+++ GNLtester/gnltest.c
@@ -1,19 +1,25 @@
 #include "../get_next_line.h"
 #include <stdio.h>
+#include <fcntl.h>
 
 int	main(int ac, char **av)
 {
 	char	*line;
+	int		fd;
+	int		tmp;
 	int		x;
 
-	x = ac;
-	//line = av[1];
-	printf("------------START-------------\n");
-	while (x >= 1)
+	x = 1;
+	printf("\n------------START-------------\n");
+	while (x <= ac)
 	{
-		if (get_next_line(ac, &line) >= 0)
-			printf("Ny rad\n");
-		x--;
+		fd = open(av[x], O_RDONLY);
+		while ((tmp = get_next_line(fd, &line) > 0))
+		{
+			if (tmp > 0)
+				printf("%s\n", line);
+		}
+		x++;
 	}
 	return (0);
 }
--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,40 @@
 /*   By: tbergkul <tbergkul@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/31 10:41:44 by tbergkul          #+#    #+#             */
-/*   Updated: 2019/11/01 16:45:04 by tbergkul         ###   ########.fr       */
+/*   Updated: 2019/11/04 15:30:47 by tbergkul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
+#include <fcntl.h>
+
+int	ft_next_line(char **str, int re, char **line, const int fd)
+{
+	char	*temp;
+	int		length;
+
+	length = 0;
+	while (str[fd][length] != '\n' && str[fd][length] != '\0')
+		length++;
+	if (str[fd][length] == '\n')
+	{
+		*line = ft_strsub(str[fd], 0, length);
+		temp = ft_strdup(str[fd] + length + 1);
+		free(str[fd]);
+		str[fd] = temp;
+		if (str[fd][0] == '\0')
+			ft_strdel(&str[fd]);
+	}
+	else if (str[fd][length] == '\0')
+	{
+		if (re == BUFF_SIZE)
+			return (get_next_line(fd, line));
+		*line = ft_strdup(str[fd]);
+		ft_strdel(&str[fd]);
+	}
+	return (1);
+}
 
 int	get_next_line(const int fd, char **line)
 {
@@ -22,34 +50,20 @@ int	get_next_line(const int fd, char **line)
 
 	if (fd < 0)
 		return (-1);
-	re = 1;
-	//printf("starting read\n");
 	while ((re = read(fd, buff, BUFF_SIZE)) > 0)
 	{
-		//printf("size read: %zu\n", re);
 		buff[re] = '\0';
-		//printf("inside read\n");
-		if (str[fd] == NULL)
-			str[fd] = ft_strnew(1);
-		//printf("strnew done\n");
-		temp = ft_strjoin((const char *)str[fd], buff); //kanske int str[fd]
+		if (!str[fd])
+			str[fd] = ft_strnew(0);
+		temp = ft_strjoin(str[fd], buff);
 		free(str[fd]);
-		//printf("free done\n");
 		str[fd] = temp;
-		//printf("str[fd] is temp\n");
 		if (ft_strchr(buff, '\n'))
-		{
-			printf("end of line\n");
 			break ;
-		}
-		//printf("end of while loop\n");
-		//printf("%s\n", str[fd]);
 	}
-	//printf("done read\n");
-	printf("TEXT:\n%s", str[fd]);
 	if (re < 0)
 		return (-1);
 	else if (re == 0 || str[fd] == NULL || str[fd][0] == '\0')
 		return (0);
-	return (0);
+	return (ft_next_line(str, re, line, fd));
 }
